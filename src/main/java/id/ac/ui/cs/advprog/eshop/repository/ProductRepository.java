45073ffc2458f91f5bd14d0cebaba4e41ab0441b
brmntyprytm <bramantyopriyou@gmail.com>
2024-02-02 10:55:23 +0700

    added ProductRepository.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -1,4 +1,4 @@
-package id.ac.ui.cs.advprog.eshop.respository;
+package id.ac.ui.cs.advprog.eshop.repository;
 
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.springframework.stereotype.Repository;
