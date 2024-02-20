3f8eea9fe7b02bc9ee110c9702abbb388d1e9341
brmntyprytm <bramantyopriyou@gmail.com>
2024-02-20 14:15:36 +0700

    refactored CarServiceImpl

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
@@ -9,7 +9,7 @@ import java.util.Iterator;
 import java.util.List;
 
 @Service
-public class CarServiceImpl {
+public class CarServiceImpl implements CarService{
     @Autowired
     private CarRepository carRepository;
 
