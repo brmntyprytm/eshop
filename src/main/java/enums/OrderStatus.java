deabb437653d13cc3187b86506d6953dafed9491
brmntyprytm <bramantyopriyou@gmail.com>
2024-03-05 07:31:47 +0700

    [REFACTOR] Add OrderStatus enum

---
+++ src/main/java/enums/OrderStatus.java
@@ -0,0 +1,22 @@
+package enums;
+
+public enum OrderStatus {
+    WAITING_PAYMENT("WAITING_PAYMENT"),
+    FAILED("FAILED"),
+    SUCCESS("SUCCESS"),
+    CANCELLED("CANCELLED");
+
+    private final String value;
+    private OrderStatus(String value) {
+        this.value = value;
+    }
+
+    public static boolean contains(String param) {
+        for (OrderStatus orderstatus : OrderStatus.values()) {
+            if (orderstatus.name().equals(param)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
